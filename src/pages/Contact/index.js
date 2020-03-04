75cf5cee25ff64d771fb20a1a67573a74e5118e9
Fernando Nicolau <fernando_nicolau@outlook.com>
2020-03-04 16:54:21 -0300

    feat: Alteração na arquitetura de pastas do projeto.

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,53 +1,24 @@
 import React, { useState } from 'react';
 import { Grid, Container, Typography, TextField, Button } from '@material-ui/core';
 import EmailIcon from '@material-ui/icons/Email';
-import Snackbar from '../../components/Snackbar/';
 import styles from './styles';
 
 function Contact() {
   const classes = styles();
 
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-      setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-      if (reason === 'clickaway') {
-          return;
-      }
-      setOpen(false);
-  };
-
   const [formData, setformData] = useState({
     name: "", email: "", subject: "", message: "", resultado:""
   });
-
-  const [captcha, setCaptcha] = useState({
-    valor1: Math.floor(Math.random() * 10), 
-    valor2: Math.floor(Math.random() * 10),
-  });
   
-
   const handleSubmit = (event) =>{   
     event.preventDefault();
-    const resultadoCaptcha = (captcha.valor1 + captcha.valor2);
-    if(resultadoCaptcha === parseInt(formData.resultado)){
-      const urlEmail = "mailto:contato-linea@linea.gov.br"+
-      "&subject="+formData.subject+
-      "&body="+
-      "<b>Nome</b>: "+formData.name+
-      "<br/> <b>Email</b>: "+formData.email+
-      "<br/><br/> <b>Mensagem</b>: </br>"+formData.message.replace(/\r?\n/g, '<br/>');
-      window.open(urlEmail);
-      setCaptcha({ 
-        valor1: Math.floor(Math.random() * 10),
-        valor2: Math.floor(Math.random() * 10)
-      });
-    }else{
-      handleClick();
-    }    
+    const urlEmail = "mailto:contato-linea@linea.gov.br"+
+    "&subject="+formData.subject+
+    "&body="+
+    "<b>Nome</b>: "+formData.name+
+    "<br/> <b>Email</b>: "+formData.email+
+    "<br/><br/> <b>Mensagem</b>: </br>"+formData.message.replace(/\r?\n/g, '<br/>');
+    window.open(urlEmail);
   }
 
   return (
@@ -90,11 +61,6 @@ function Contact() {
 
               <Grid container alignItems="flex-end">
                 <Grid item xs={10}>
-                  <div>
-                    <TextField required id="textFieldResultado" type="number" variant="outlined"
-                    value={formData.resultado} onChange={(event) => setformData({ ...formData, resultado: event.target.value})}
-                    label={`Resultado de ${captcha.valor1} + ${captcha.valor2}: `} placeholder="Resultado" size="small"/>
-                  </div>
                 </Grid>
                 <Grid item xs={2}>
                   <Button variant="contained" color="primary" type="submit" disableElevation>
@@ -105,7 +71,6 @@ function Contact() {
               
             </form>
           </Grid>
-          <Snackbar open={open} handleClose={handleClose} />
       </Container>
     </div>
   );
