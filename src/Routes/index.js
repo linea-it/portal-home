5c582fb0a239bfb468dc5c51fa2b524bef4d1cb6
Fernando <fernando_nicolau@outlook.com>
2020-04-07 14:03:13 -0300

    feat: Adding About us route.

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import Header from '../components/Header';
 import Home from '../pages/Home';
 import AboutUs from '../pages/AboutUs';
-=======
-
-import Header from '../components/Header';
-import Home from '../pages/Home';
->>>>>>> c3309154e2b850a8eb03a3c694a6f4ca3e47083c
 import Tutorials from '../pages/Tutorials';
 import Contact from '../pages/Contact';
 
@@ -18,10 +12,7 @@ function Router() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
-<<<<<<< HEAD
         <Route path="/about-us" component={AboutUs} />
-=======
->>>>>>> c3309154e2b850a8eb03a3c694a6f4ca3e47083c
         <Route path="/tutorials" component={Tutorials} />
         <Route path="/contact" component={Contact} />
       </Switch>
